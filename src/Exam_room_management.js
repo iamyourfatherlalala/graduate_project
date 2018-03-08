f4385ae21476c616ee247affbb47d42d90bddac2
王佳祺 <stefanmao@wangjiaqideMacBook-Pro.local>
2018-03-08 16:18:37 +0800

    solve setState undefined, fetch data from server, display data on the screen

--- src/Exam_room_management.js
+++ src/Exam_room_management.js
@@ -5,34 +5,87 @@ import fetch from 'isomorphic-fetch';
 import { browserHistory } from 'react-router'
 import { BrowserRouter as Router, Route, IndexRedirect } from 'react-router-dom';
 import { token } from './Login';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
 
 class Exam_room_management extends Component {
     constructor(props) {
         super(props);
-        this.states = {
-            exam_conditions : []
-        }
+        this.state = {
+            exam_conditions : [],
+         }
     }
 
-    async handleButton() {
-        const response = await fetch(`http://58.196.130.215/EMS/admin/roomLists?token=${token}`);
-        const result = await response.json();
-        console.log(result[0].id);
-       // this.tests = JSON.parse(JSON.stringify(result));
-        // this.tests = result;
-        console.log(this.tests);
-    }
+    // componentWillMount() {
+    //     this.handleButton();
+    // }
+
+     handleButton = () => {
+         let url = `http://58.196.130.215/EMS/admin/roomLists?token=${token}`;
+         fetch(url)
+         .then(function(response) {
+             if (!response.ok) {
+                 throw Error(response.statusText);
+                }
+                return response;
+            }).then((response) => {
+                response.json().then((data) => {
+                     this.setState({exam_conditions: data});
+                    // alert(this.state.exam_conditions[0].id);
+                });
+             }).catch((error) => {
+                 console.log(error);
+                });
+            }
 
     render() {
-        if (!this.tests[0]) {
+        if (!this.state.exam_conditions[0]) {
             return (
                 <button onClick={this.handleButton}>give out papers</button>
             );
-        } else {
+         } 
+         else {
+            const { exam_conditions } = this.state;
             return (
-                <div>
-                    {this.state.tests}
-                </div>
+                <ReactTable
+                data={exam_conditions}
+                columns={[
+                  {
+                    Header: "Id",
+                    accessor: "id"
+                  },
+                  {
+                    Header: "RegisteredNum",
+                    accessor: "registeredNum"
+                  },
+                  {
+                    Header: "RoomName",
+                    accessor: "roomName"
+                  },
+                  {
+                    Header: "StartTime",
+                    accessor: "startTime"
+                  },
+                  {
+                    Header: "Status",
+                    accessor: "status"
+                  },
+                  {
+                    Header: "TestNum",
+                    accessor: "testNum"
+                  },
+                  {
+                    Header: "Uid",
+                    accessor: "uid"
+                  },
+                  {
+                    Header: "UnRegisteredNum",
+                    accessor: "unRegisteredNum"
+                  }
+                ]}
+                defaultPageSize={10}
+                className="-striped -highlight"
+              />
             )
         }
     }
